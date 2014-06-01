17c8f03d29699367b45b68a1a33941bfb7f7f060
Thanh Nguyen <btnguyen2k@gmail.com>
2014-06-02 01:35:07 +0700

    Page: author list

--- modules/admin/app/controllers/admin/Admin.java
+++ modules/admin/app/controllers/admin/Admin.java
@@ -5,6 +5,8 @@ import play.libs.F.Function0;
 import play.libs.F.Promise;
 import play.mvc.Result;
 import truyen.common.Constants;
+import truyen.common.bo.truyen.AuthorBo;
+import truyen.common.bo.truyen.TruyenDao;
 import truyen.common.compisitions.AuthRequired;
 import controllers.common.BaseController;
 
@@ -14,11 +16,29 @@ public class Admin extends BaseController {
 
     public final static String SECTION = "admin.";
     public final static String VIEW_INDEX = SECTION + "index";
+    public final static String VIEW_AUTHORS = SECTION + "authors";
 
+    /*
+     * Handles GET:/index
+     */
     public static Promise<Result> index() {
         Promise<Result> promise = Promise.promise(new Function0<Result>() {
             public Result apply() throws Exception {
                 Html html = render(VIEW_INDEX);
+                return ok(html);
+            }
+        });
+        return promise;
+    }
+
+    /*
+     * Handles GET:/authors
+     */
+    public static Promise<Result> authors() {
+        Promise<Result> promise = Promise.promise(new Function0<Result>() {
+            public Result apply() throws Exception {
+                AuthorBo[] authors = TruyenDao.getAllAuthors();
+                Html html = render(VIEW_AUTHORS, (Object)authors);
                 return ok(html);
             }
         });
