83ec00531627c2d44768ccaeeb478bacc2aec7c7
Thanh Nguyen <btnguyen2k@gmail.com>
2014-08-07 11:11:21 +0700

    Fix merge

--- project/Build.scala
+++ project/Build.scala
@@ -6,15 +6,39 @@ import com.typesafe.sbteclipse.core.EclipsePlugin.EclipseKeys
 
 object ApplicationBuild extends Build {
     override def settings = super.settings ++ Seq(
-      EclipseKeys.skipParents in ThisBuild := false)
+        EclipseKeys.skipParents in ThisBuild := false
+    )
 
-    val conf = ConfigFactory.parseFile(new File("conf/application.conf")).resolve()
-    val appName = "vngup-rtreports"
-    val appVersion = conf.getString("app.version")
+    val conf            = ConfigFactory.parseFile(new File("conf/application.conf")).resolve()
+    val appName         = "vngup-rtreports"
+    val appVersion      = conf.getString("app.version")
+    
+    val _javaVersion = "1.6"
+      
+    val appDependenciesBase = Seq(
+        javaJdbc,
+        cache,
+        filters,
+        "org.slf4j"             %  "log4j-over-slf4j"       % "1.7.7",
+        "mysql"                 %  "mysql-connector-java"   % "5.1.29",
+        "com.google.guava"      %  "guava"                  % "16.0.1",
+        "org.apache.commons"    %  "commons-pool2"          % "2.2",
+        "org.jodd"              %  "jodd-http"              % "3.5.2",
+        "org.jodd"              %  "jodd-lagarto"           % "3.5.2",
+        "com.ibm.icu"           %  "icu4j"                  % "53.1",
+        "com.github.ddth"       %  "ddth-commons"           % "0.2.2.2",
+        "com.github.ddth"       %  "spring-social-helper"   % "0.2.1",
+        "com.github.ddth"       %  "ddth-tsc"               % "0.5.0",
+        "com.github.ddth"       %  "ddth-tsc-cassandra"     % "0.5.0",
+        "com.github.ddth"       %  "ddth-tsc-redis"         % "0.5.0",
+        "com.github.ddth"       %% "play-module-plommon"    % "0.5.1.5"
+    )
+    
     val appDepsCommon = appDependenciesBase ++ Seq(
         "org.springframework.security" % "spring-security-core" % "3.2.4.RELEASE",
         "org.springframework.security" % "spring-security-ldap" % "3.2.4.RELEASE"
     )
+    
     val moduleCommon = play.Project(
         appName + "-common", appVersion, appDepsCommon, path = file("modules/common")
     ).settings(
